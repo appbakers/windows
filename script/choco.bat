fe0502e2fc70cc1dbd1b61c65696866d84475a27
appbakers <greeny100@gmail.com>
2016-08-23 10:41:34 +0900

    *auto script/choco.bat

--- script/choco.bat
+++ script/choco.bat
@@ -1,3 +1,6 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "winrm set winrm/config/winrs '@{MaxShellsPerUser=100}'"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "winrm set winrm/config '@{MaxTimeoutms="7200000"}'"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="0"}'"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "winrm set winrm/config/winrs '@{MaxProcessesPerShell="0"}'"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "winrm set winrm/config/winrs '@{MaxShellsPerUser="0"}'"
