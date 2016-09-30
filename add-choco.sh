4532c65c22360800f9fda51cf675f559f252c14e
appbakers <greeny100@gmail.com>
2016-09-30 11:51:23 +0900

    Revert "* add-choco.sh to add script/choco.bat to * packer json file"
    This reverts commit 4dd8e476253f098ba2dc5a4f12eab98bdb48799d.

--- add-choco.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# simple script to add script/choco.bat before clean.bat
-#
-cat > script/choco.bat <<EOF
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-EOF
-
-ls -al *.json | awk '{print $9}' | xargs -I{} sed -i 's/\"script\/clean.bat/"script\/choco.bat",\n        "script\/clean.bat/' {}
